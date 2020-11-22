9f931e03adb03b6d1a89b8c9085ee305058a38ae
Faqih Muntashir <itsfaqih@gmail.com>
2020-11-22 07:31:44 +0700

    Animated bullets in project section

--- src/components/organisms/Projects/index.tsx
+++ src/components/organisms/Projects/index.tsx
@@ -19,7 +19,7 @@ export default function Projects({ title, content, className }: ProjectsSectionP
   const language = useContext(LanguageContext);
   const { items } = content;
 
-  const bulletsClasses = 'absolute w-32 h-32 text-indigo-300 md:w-40 md:h-40';
+  const bulletsClasses = 'w-32 h-32 text-indigo-300 md:w-40 md:h-40';
 
   useEffect(() => {
     if (inView) {
@@ -44,15 +44,24 @@ export default function Projects({ title, content, className }: ProjectsSectionP
       <SearchBox className="mx-auto mt-6 transition-all transform sm:w-2/3 md:w-1/2 lg:w-1/3" />
 
       <div className="relative grid grid-cols-1 gap-8 mt-6 sm:grid-cols-2 lg:grid-cols-3">
-        <Decorations.Bullets
-          className={classNames('-top-8 -left-12 md:-top-9 md:-left-16 -z-10', bulletsClasses)}
-        />
-        <Decorations.Bullets
-          className={classNames(
-            '-bottom-8 -right-12 md:-bottom-9 md:-right-16 -z-10',
-            bulletsClasses
-          )}
-        />
+        <Animated.FromDirection
+          from="top"
+          animate={controls}
+          custom={1}
+          delay={1}
+          className="absolute -top-8 -left-12 md:-top-9 md:-left-16 -z-10"
+        >
+          <Decorations.Bullets className={bulletsClasses} />
+        </Animated.FromDirection>
+        <Animated.FromDirection
+          from="bottom"
+          animate={controls}
+          custom={1}
+          delay={1}
+          className="absolute -bottom-8 -right-12 md:-bottom-9 md:-right-16 -z-10"
+        >
+          <Decorations.Bullets className={bulletsClasses} />
+        </Animated.FromDirection>
         {items.map((item, index) => (
           <Animated.FadeIn key={index} animate={controls} custom={index} delay={0.2}>
             <ProjectCard
@@ -62,13 +71,12 @@ export default function Projects({ title, content, className }: ProjectsSectionP
           </Animated.FadeIn>
         ))}
       </div>
-      
-        <Animated.FromDirection from="bottom" animate={controls} custom={2} delay={1}>
-          <RoundedButton className="mx-auto mt-8">
-            {language.value === 'en' ? 'See more' : 'Lihat Selengkapnya'}
-          </RoundedButton>
-        </Animated.FromDirection>
-      
+
+      <Animated.FromDirection from="bottom" animate={controls} custom={2} delay={1}>
+        <RoundedButton className="mx-auto mt-8">
+          {language.value === 'en' ? 'See more' : 'Lihat Selengkapnya'}
+        </RoundedButton>
+      </Animated.FromDirection>
     </section>
   );
 }
