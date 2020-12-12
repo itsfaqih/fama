903ac0d62f3a65a2191795be0f2636d4ad9f5414
Faqih Muntashir <itsfaqih@gmail.com>
2020-12-12 20:53:09 +0700

    Removed resume file

--- src/components/organisms/About/index.tsx
+++ src/components/organisms/About/index.tsx
@@ -63,7 +63,7 @@ const About = forwardRef<HTMLElement | undefined, AboutSectionProps>(
           </Animated.FromDirection>
           <div className="mx-auto mt-6 lg:mr-0 lg:ml-auto lg:mt-0">
             <Animated.FromDirection from="bottom" animate={controls} custom={1} delay={1}>
-              <RoundedButton as="a" href="./resume.pdf">
+              <RoundedButton as="a" href="./#">
                 {language.value === 'en' ? 'Download' : 'Unduh'} Resume
               </RoundedButton>
             </Animated.FromDirection>
