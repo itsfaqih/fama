febce528b6fd6ce6e6c4db64419d72a50ed415d8
Faqih Muntashir <itsfaqih@gmail.com>
2020-12-14 17:35:43 +0700

    Modified TitleLine

--- src/components/atoms/TitleLine/index.tsx
+++ src/components/atoms/TitleLine/index.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import { motion, SVGMotionProps, Variants } from 'framer-motion';
 import classNames from 'classnames';
+import { HTMLAttributes } from 'react';
 
-export default function TitleLine({ className, ...props }: SVGMotionProps<SVGElement>) {
-  const variants: Variants = {
-    show: {
-      width: '100%',
-    },
-    hidden: {
-      width: 0,
-    },
-  };
-
+export default function TitleLine({ className, ...props }: HTMLAttributes<SVGElement>) {
   return (
     <svg className={classNames('w-20 h-1 text-indigo-600 mt-3', className)} fill="currentColor">
-      <motion.rect variants={variants} initial="hidden" animate="show" height="100%" {...props} />
+      <rect height="100%" {...props} />
     </svg>
   );
 }
