b43b29a79cf5e2649f4997b7ddea3f3c3141d53b
Faqih Muntashir <itsfaqih@gmail.com>
2020-12-14 17:17:39 +0700

    Removed about animation

--- src/components/organisms/About/index.tsx
+++ src/components/organisms/About/index.tsx
@@ -4,31 +4,18 @@ import { SectionTitle } from 'components/molecules';
 import { LanguageContext } from 'contexts';
 import React, { useContext } from 'react';
 import AboutSectionProps from 'components/organisms/About/types';
-import { useAnimation } from 'framer-motion';
 import { useEffect } from 'react';
-import { useInView } from 'react-intersection-observer';
 import { forwardRef } from 'react';
 
 const About = forwardRef<HTMLElement | undefined, AboutSectionProps>(
   ({ title, content, className }: AboutSectionProps, ref) => {
-    const controls = useAnimation();
-    const [refView, inView] = useInView({
-      threshold: 0.5,
-    });
     const language = useContext(LanguageContext);
-
-    useEffect(() => {
-      if (inView) {
-        controls.start('show');
-      }
-    }, [controls, inView]);
 
     return (
       <section
         id="about"
         className={className}
         ref={section => {
-          refView(section);
           if (ref) {
             if (typeof ref === 'function') {
               ref(section);
@@ -38,35 +25,15 @@ const About = forwardRef<HTMLElement | undefined, AboutSectionProps>(
           }
         }}
       >
-        <SectionTitle
-          lineProps={{
-            animate: controls,
-            custom: 1,
-            transition: {
-              delay: 1,
-            },
-          }}
-        >
-          <Animated.Letter text={title} animate={controls} custom={0} delay={1} />
-        </SectionTitle>
+        <SectionTitle>{title}</SectionTitle>
         <div className="grid grid-rows-2 mt-6 lg:grid-cols-3 lg:grid-rows-1">
-          <Animated.FromDirection
-            className="lg:col-span-2"
-            from="left"
-            animate={controls}
-            custom={0}
-            delay={1}
-          >
-            <Animated.Reveal from="left" animate={controls} custom={1} delay={1}>
-              <ContentText className="lg:text-left">{content.text}</ContentText>
-            </Animated.Reveal>
-          </Animated.FromDirection>
+          <div className="lg:col-span-2">
+            <ContentText className="lg:text-left">{content.text}</ContentText>
+          </div>
           <div className="mx-auto mt-6 lg:mr-0 lg:ml-auto lg:mt-0">
-            <Animated.FromDirection from="bottom" animate={controls} custom={1} delay={1}>
-              <RoundedButton as="a" href="./#">
-                {language.value === 'en' ? 'Download' : 'Unduh'} Resume
-              </RoundedButton>
-            </Animated.FromDirection>
+            <RoundedButton as="a" href="./#">
+              {language.value === 'en' ? 'Download' : 'Unduh'} Resume
+            </RoundedButton>
           </div>
         </div>
       </section>
