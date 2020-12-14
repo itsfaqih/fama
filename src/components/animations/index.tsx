9cacc0db31800c23b9449b4274dde0e17a2e9fd4
Faqih Muntashir <itsfaqih@gmail.com>
2020-12-14 17:42:05 +0700

    Removed unused animation components

--- src/components/animations/index.tsx
+++ src/components/animations/index.tsx
@@ -1,10 +1,5 @@
-import Letter from 'components/animations/Letter';
-import Highlighter from 'components/animations/Highlighter';
 import FromDirection from 'components/animations/FromDirection';
-import Reveal from 'components/animations/Reveal';
-import FadeIn from 'components/animations/FadeIn';
-import StaggerChildren from 'components/animations/StaggerChildren';
 
-const Animated = { Letter, Highlighter, FromDirection, Reveal, FadeIn, StaggerChildren };
+const Animated = { FromDirection };
 
 export default Animated;
