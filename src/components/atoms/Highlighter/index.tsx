bc6a6691e3c270d5f94921bb343bf1c3a793d7bf
Faqih Muntashir <itsfaqih@gmail.com>
2020-12-14 17:38:31 +0700

    Fixed highlighter width

--- src/components/atoms/Highlighter/index.tsx
+++ src/components/atoms/Highlighter/index.tsx
@@ -11,7 +11,7 @@ export default function Highlighter({ className, rad, ...props }: Props) {
       className={classNames('absolute w-full-2 text-indigo-200 -z-10 -left-1', className)}
       fill="currentColor"
     >
-      <rect height="100%" rx={rad} ry={rad} {...props} />
+      <rect width="100%" height="100%" rx={rad} ry={rad} {...props} />
     </svg>
   );
 }
